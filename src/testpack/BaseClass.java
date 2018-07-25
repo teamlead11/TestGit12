23ddf7593323e7d74350960acd94dc6642083240
raknew <rakeshnewone100@gmail.com>
2018-07-25 10:53:02 +0530

    String Reverse fuction siddharthanStringReverse Added

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -4,10 +4,27 @@ public class BaseClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		siddharthaStringReverse("Siddharthan Prabakaran");
 	}
 
 	public static void mymethod() {
 		System.out.println("my method");
 	}
+	
+	public static void siddharthaStringReverse(String a)
+	{
+
+		int i = a.length();
+		int j;
+		
+		char[] reverseArray = new char[i];
+		
+		for(i= i-1,j=0; i>=0; i--,j++)
+		{
+			reverseArray[j] = a.charAt(i);	
+		}
+		
+		System.out.println(reverseArray);
+	}
+	
 }
