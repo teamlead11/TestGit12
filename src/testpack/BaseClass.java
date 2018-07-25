94a763b168a039ce90a5a5bc43ddf50a212789e9
vetrikrrish <vetrikrrish@gmail.com>
2018-07-25 10:34:31 +0530

    added string reverse for the brnach vetri

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -1,13 +1,32 @@
 package testpack;
 
+import java.util.Scanner;
+
 public class BaseClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		VetriStringReverse();
 
 	}
 
 	public static void mymethod() {
 		System.out.println("my method");
 	}
+	public static void VetriStringReverse() {
+		
+        System.out.println("Enter string to reverse:");
+        
+        Scanner read = new Scanner(System.in);
+        String str = read.nextLine();
+        String reverse = "";
+        
+        
+        for(int i = str.length() - 1; i >= 0; i--)
+        {
+            reverse = reverse + str.charAt(i);
+        }
+        
+        System.out.println("Reversed string is:");
+        System.out.println(reverse);
+    }
 }
