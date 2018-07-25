b1d2c5e3463de6c883dafbbc739f8d16633d328a
likinani <nithya4a2@gmail.com>
2018-07-25 10:52:50 +0530

    addding reverse string prgm

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -4,10 +4,17 @@ public class BaseClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		lohithaStringReverse();
 	}
 
-	public static void mymethod() {
-		System.out.println("my method");
+	public static void lohithaStringReverse() {
+		String input = "Welcome to Java";
+        StringBuilder input1 = new StringBuilder();
+        input1.append(input);
+        input1 = input1.reverse();
+        System.out.println(input1);
+ 
+       
+	
 	}
 }
