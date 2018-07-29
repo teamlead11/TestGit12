1c31019a674b984ed7ba6292ea607bbc3b72ebb4
SoumenduKhilar <soumendu1992@gmail.com>
2018-07-29 11:45:09 +0530

    Added comments

--- src/testpack/LifeTimeLife.java
+++ src/testpack/LifeTimeLife.java
@@ -21,13 +21,17 @@ public class LifeTimeLife {
 	@Test
 	public void LifeTime() throws InterruptedException {
 		driver.get("https://www.lifetime.life/");
+		//Print the Menu in lifetime.life Homepage.
 		List<WebElement> elements = driver.findElements(By.xpath("//ul[contains(@id,'primaryNav')]//following-sibling::li[contains(@class,'nav-item')]"));
 		for (WebElement webElement : elements) {
 			System.out.println(webElement.getText());			
 		}
+		
+		//Click on Sports and then Basketball.
 		driver.findElement(By.xpath("//a[@href='/sports-programs.html']")).click();
 		driver.findElement(By.xpath("//ul[@class='list-unstyled dropdown-columns']//child::a[@href='/sports-programs/basketball.html']")).click();
 		
+		//To Scroll down to "Premier Basketball Courts" and print it. (Doesn't work, need to check the code.)
 		JavascriptExecutor j = (JavascriptExecutor) driver;
 		WebElement elements1 = driver.findElement(By.xpath("//h4[text()='Premier Basketball Courts']"));
 	    
