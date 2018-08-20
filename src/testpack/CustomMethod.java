db370c38259ed0f7368fb05c332146edbfe8172e
amrirajan <rajanvc7@gmail.com>
2018-08-20 13:21:08 +0530

    adding custom method

--- src/testpack/CustomMethod.java
+++ src/testpack/CustomMethod.java
@@ -1,46 +1,47 @@
 package testpack;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-public class CustomMethod {
-	public static void main(String[] args) throws Throwable {
-		
-		addToCartBasedOnIndex(1);
-		
-	}
 	
+	import java.util.ArrayList;
+	import java.util.List;
 	
-	public static void addToCartBasedOnIndex(int index) throws Throwable {
+	import org.openqa.selenium.By;
+	import org.openqa.selenium.Keys;
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.WebElement;
+	import org.openqa.selenium.chrome.ChromeDriver;
+	
+	public class CustomMethod{
+		public static void main(String[] args) throws InterruptedException {
+			
+			flipkart(1);
 		
+			}
 		
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Am Gracy\\git\\TestGit12\\driver\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.flipkart.com/");
-		driver.manage().window().maximize();
-		
-		Thread.sleep(3000);
-		
+		public static void flipkart(int index) throws InterruptedException{
+			System.setProperty("webdriver.chrome.driver", "C:\\\\Users\\\\Rajan\\\\eclipse-workspace\\\\AdditionSample\\\\driver\\\\chromedriver.exe");
+			WebDriver driver=new ChromeDriver();
+			driver.get("https://www.flipkart.com/");
+			driver.manage().window().maximize();
 		driver.findElement(By.xpath("//button[@class='_2AkmmA _29YdH8']")).click();
-		driver.findElement(By.xpath("//input[@placeholder='Search for products, brands and more']")).sendKeys("i Phone X",Keys.ENTER);
-		
-		Thread.sleep(5000);
-		
-		List<WebElement> elements = driver.findElements(By.xpath("//div[@class='_3wU53n']"));
-		
-		elements.get((index-1)).click();
-	   
-		ArrayList<String> tabs2 = new ArrayList<String> (driver.getWindowHandles());
-	    driver.switchTo().window(tabs2.get(1));
-		
-		driver.findElement(By.xpath("//button[@class='_2AkmmA _2Npkh4 _2MWPVK']")).click();
-				
-	}
+			driver.findElement(By.className("LM6RPg")).sendKeys("iphone 6",Keys.ENTER);
+			Thread.sleep(3000);
+		WebElement p=driver.findElement(By.xpath("//div[@class='_1HmYoV _35HD7C col-10-12']"));
+		List<WebElement> element=p.findElements(By.xpath("//div[@class='_3wU53n']"));
+		element.get((index-1)).click();
+		System.out.println(element.size());
+			for(int i = 0; i<element.size(); i++){
+						if(element.get(i).getText().equals(index)){
+					element.get(i).click();
+					break;
+				}
+			}
 	
-}
+	ArrayList<String> s=new ArrayList<String> (driver.getWindowHandles()) ;
+	driver.switchTo().window(s.get(1));
+	System.out.println(driver.getTitle());
+	driver.findElement(By.xpath("(//li[@class='col col-6-12'])[1]")).click();
+		}}
+
+
+	
+
+	
